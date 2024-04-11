b0aa1f0e1dd3975c27f7ff7eb3ce9d9caa6d167c
atucci <alessiotucci33@gmail.com>
2024-04-11 12:10:21 +0200

    finishing up the works

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 11:50:34 by atucci            #+#    #+#             */
+/*   Updated: 2024/04/11 12:00:12 by atucci           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	getNbAccounts( void ) {}
+
+static int	getTotalAmount( void ) {}
+
+static int	getNbDeposits( void ) {}
+
+static int	getNbWithdrawals( void ) {}
+
+static void	displayAccountsInfos( void ) {}
+
+Account( int initial_deposit ) {}
+
+~Account( void ) {}
+
+void	makeDeposit( int deposit ) {}
+
+bool	makeWithdrawal( int withdrawal ) {}
+
+int		checkAmount( void ) const;
+
+void	displayStatus( void ) const;
+
+
+static void	_displayTimestamp( void ){}
