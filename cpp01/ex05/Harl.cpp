2d30d42fd4a080d136a3736b28fc98b81b8ce664
Alessio Tucci <alessiotucci33@gmail.com>
2024-04-13 13:42:11 +0200

    working on harl and pointer to function

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,8 +6,38 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 19:07:19 by atucci            #+#    #+#             */
-/*   Updated: 2024/04/12 19:07:21 by atucci           ###   ########.fr       */
+/*   Updated: 2024/04/13 13:40:47 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
 
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+
+void Harl::_debug()
+{
+	cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!";
+}
+
+void Harl::_info()
+{
+	cout << GREEN << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << RESET;
+}
+
+void Harl::_warning()
+{
+	cout << YELLOW << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << RESET;
+}
+
+void Harl::_error()
+{
+	cout << RED << "This is unacceptable! I want to speak to the manager now." << RESET;
+}
+
+void Harl::complain( std::string level)
+{
+	
+}
--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -6,8 +6,37 @@
 /*   By: atucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 19:07:29 by atucci            #+#    #+#             */
-/*   Updated: 2024/04/12 19:07:31 by atucci           ###   ########.fr       */
+/*   Updated: 2024/04/13 13:40:27 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HARL_HPP
+#define HARL_HPP
+# define BLUE    "\033[1;34m"
+# define PURPLE  "\033[1;35m"
+# define GRAY    "\033[1;30m"
+# define GREEN   "\033[1;32m"
+# define CYAN    "\033[1;36m"
+# define YELLOW  "\033[1;33m"
+# define RED     "\033[1;31m"
+# define RESET   "\033[0m"
+# define BG_RESET        "\033[49m"
+# define BG_GREEN        "\033[42m"
+# define BG_CYAN         "\033[46m"
+# define BG_YELLOW       "\033[43m"
+# define BG_RED          "\033[41m"
+#include <iostream>
 
+class Harl
+{
+public:
+	void	complain(std::string level);
+	Harl();// not sure about that
+	~Harl();
+private:
+	void	_debug(void);
+	void	_info(void);
+	void	_warning(void);
+	void	_error(void);
+};
+#endif
