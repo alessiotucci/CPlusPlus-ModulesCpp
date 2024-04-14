0cbaa945c9cd41a9b670c12acf3462bce5b8c3f5
atucci <alessiotucci33@gmail.com>
2024-04-14 18:02:01 +0200

    done for the day

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,8 +6,60 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 14:46:00 by atucci            #+#    #+#             */
-/*   Updated: 2024/04/14 14:46:02 by atucci           ###   ########.fr       */
+/*   Updated: 2024/04/14 18:00:03 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+// 1. Constructor
+ClapTrap::ClapTrap(std::string name)
+{
+	std::cout << "ClapTrap default constructor called" << std::endl;
+	this->_hitPoints = 10;
+	this->_energyPoints = 10;
+	this->_attackDamage = 0;
+}
+
+// 2. Destructor
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap destructor called" << std::endl;
+}
+
+// 3. Copy Constructor
+ClapTrap::ClapTrap(const ClapTrap& other)
+{
+	std::cout << "ClapTrap copy constructor called" << std::endl;
+	*this = other;
+}
+
+// 4. Copy Assignment Operator
+ClapTrap::ClapTrap& operator=(const ClapTrap& other)
+{
+	std::cout << "ClapTrap copy assignment operator called" << std::endl;
+	if (this != &other)
+	{
+		// Copy each property from other to this
+		// For example: this->property = other.property;
+		this->_name = other._name;
+		this->_hitPoints = other._hitPoints;
+		this->_energyPoints = other._energyPoints;
+		this->_attackDamage = other._attackDamage;
+	}
+	return *this;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+
+}
+void ClapTrap::beRepaired(unsigned int amount)
+{
+
+}
 
--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,8 +6,31 @@
 /*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 14:46:12 by atucci            #+#    #+#             */
-/*   Updated: 2024/04/14 14:46:14 by atucci           ###   ########.fr       */
+/*   Updated: 2024/04/14 17:49:47 by atucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
 
+class ClapTrap
+{
+	public:
+		//1) CONSTRUCTOR
+		ClapTrap(std::string name);
+		//2) DESTRUCTOR
+		~ClapTrap();
+		//3) COPY CONSTRUCTOR
+		ClapTrap(const ClapTrap& other);
+		//4) COPY ASSIGNMENT OPERATOR
+		ClapTrap& operator=(const ClapTrap& other);
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+	private:
+		std:string _name;
+		int _hitPoints;
+		int _energyPoints;
+		int _attackDamage;
+};
+#endif
