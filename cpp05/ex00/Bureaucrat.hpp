97ceaacf6026c1e11290a21f8c4050ddcab53859
Alessio Tucci <alessiotucci33@gmail.com>
2025-03-05 11:19:30 +0100

    started off with main, and remember the other cpp notable stuff

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -1,1 +1,54 @@
-//TODO
+/*****************************************************************************
+*You should be able to use each of your headers independently from others.   *
+*Thus, they must include all the dependencies they need.                     *
+*However, you must avoid the problem of double inclusion by adding include guards.
+*Otherwise, your grade will be 0.                                            *
+*****************************************************************************/
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+
+//TODO:
+/*A Bureaucrat must have:
+ * • a constant name.
+ * • a grade that ranges from 1 (highest) to 150 (lowest).
+*/
+
+//TODO:
+/*You will provide getters for both these attributes:
+ * getName();
+ * getGrade();
+ * Implement also two member functions;
+ * increment the bureaucrat grade.
+ * decrement the bureaucrat grade.
+*/
+
+//TODO:
+/*Implement an overload of the insertion («) operator to print something like*/
+
+//TODO:
+/* If the grade is out of range,
+ * both of them will throw the same exceptions as the constructor.*/
+class Bureaucrat
+{
+	private:
+		//
+		//
+	public:
+		Bureaucrat ();
+		Bureaucrat (const Bureaucrat &other);
+		~Bureaucrat ();
+		Bureaucrat & operator = (const Bureaucrat aother);
+		// getters
+		// function to increment
+		// overload operator
+};
+
+// https://www.francescmm.com/orthodox-canonical-class-form/
+// implement the class in canonical form
+// CONSTRUCTOR
+// DESTRUCTOR
+// COPY COSTRUCTOR
+//
+#endif
--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -1,4 +1,18 @@
+//TODO:
+// for now writing just some test
+#include <iostream>
 int	main()
 {
+	std::cout << "TEST0" << std::endl;
+	std::cout << "TEST1" << std::endl;
+	std::cout << "TEST2" << std::endl;
+	std::cout << "TEST3" << std::endl;
+	std::cout << "TEST4" << std::endl;
+	std::cout << "TEST5" << std::endl;
+	std::cout << "TEST6" << std::endl;
+	std::cout << "TEST7" << std::endl;
+	std::cout << "TEST8" << std::endl;
+	std::cout << "TEST9" << std::endl;
+	std::cout << "TEST9" << std::endl;
 	return (0);
 }
