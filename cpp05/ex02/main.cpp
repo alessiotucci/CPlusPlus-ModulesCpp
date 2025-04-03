85f6a6db151e3ecf1a4bb4df8d3e6ed6d94bdb15
Alessio Tucci <alessiotucci33@gmail.com>
2025-04-03 14:17:25 +0200

    let

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -4,6 +4,19 @@
 
 int main()
 {
+	Bureaucrat	bob(120, "Bob");
+	std::cout << bob << std::endl;
+
+	std::cout << "TEST 1: " << std::endl;
+	std::cout << "TEST 2: " << std::endl;
+	std::cout << "TEST 3: " << std::endl;
+	std::cout << "TEST 4: " << std::endl;
+	std::cout << "TEST 5: " << std::endl;
+	std::cout << "TEST 6: " << std::endl;
+	std::cout << "TEST 7: " << std::endl;
+	std::cout << "TEST 8: " << std::endl;
+	std::cout << "TEST 9: " << std::endl;
+	std::cout << "TEST 10: " << std::endl;
 	return 0;
 }
 
