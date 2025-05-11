a945543d06f4047cf86c0672892d7e3078a5f590
Alessio Tucci <alessiotucci33@gmail.com>
2025-05-11 19:40:00 +0200

    almost done

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -24,14 +24,44 @@ Intern& Intern::operator=(const Intern& other)
 Intern::~Intern() { }
 
 
+
 //TODO: implement this function
+// options are:
+//1) robotomy request
+//2) presidential pardon
+//3) shrubbery creation
+// Helper to map form names to an integer code
+static int formCode(const std::string &name)
+{
+	if (name == "shrubbery creation")
+		return 0;
+	if (name == "robotomy request")
+		return 1;
+	if (name == "presidential pardon")
+		return 2;
+	return (-1);
+}
 AForm* Intern::makeForm(std::string nameForm, std::string target)
 {
-	(void)nameForm; (void)target;
-	AForm *form;
-	form = 0;
-	return (form);
 //It will print something like:
 //Intern creates <form>
 //If the form name passed as parameter doesn’t exist, print an explicit error message.
+	int code = formCode(nameForm);
+	AForm *form = NULL;
+	switch (code)
+	{
+		case 0:
+			form = new ShrubberyCreationForm(target);
+			break;
+		case 1:
+			form = new RobotomyRequestForm(target);
+			break;
+		case 2:
+			form = new PresidentialPardonForm(target);
+			break;
+		default:
+			throw NonExistentRequestException();
+	}
+	std::cout << "Intern creates [" << form->getName() << "]" <<  std::endl;
+	return form;
 }
