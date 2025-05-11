98de0ac12787fa8e7de630c3c2119aa73d79a909
Alessio Tucci <alessiotucci33@gmail.com>
2025-05-11 19:29:21 +0200

    added small changes

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -2,28 +2,35 @@
 #include "AForm.hpp"
 //TODO:
 
-Inter::Intern()
+Intern::Intern()
 { }
 
 // 2. Copy constructor
-Inter::Intern(const Intern& other)
+Intern::Intern(const Intern& other)
 {
-
+	*this = other;
+	return ;
 }
 
 // 3. Copy assignment operator
 Intern& Intern::operator=(const Intern& other)
 {
-
+	if (this == &other)
+		return (*this);
+	return (*this);
 }
 
 // 4. Destructor
-Inter::~Intern() { }
+Intern::~Intern() { }
 
 
 //TODO: implement this function
 AForm* Intern::makeForm(std::string nameForm, std::string target)
 {
+	(void)nameForm; (void)target;
+	AForm *form;
+	form = 0;
+	return (form);
 //It will print something like:
 //Intern creates <form>
 //If the form name passed as parameter doesn’t exist, print an explicit error message.
