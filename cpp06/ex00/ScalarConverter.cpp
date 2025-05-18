ba975a4cda8c25601f03c4d8c9b1f1bded43165c
Alessio Tucci <alessiotucci33@gmail.com>
2025-05-18 18:11:43 +0200

    starting

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -17,10 +17,38 @@ ScalarConverter& ScalarConverter::operator=( const ScalarConverter& src)
 	return (*this);
 }
 
+static void displayResult(char a, int b, float c, double d)
+{
+	std::cout << "char: " << a << std::endl;
+	std::cout << "int: " << b << std::endl;
+	std::cout << "float: " << c << std::endl;
+	std::cout << "double: " << d << std::endl;
+}
+static bool looksLikeChar(const std::string string) { (void)string; return true; }
+static void handleChar(const std::string str) { (void)str; return; }
+
+static bool looksLikeInt(const std::string string) { (void)string; return true; }
+static void handleInt(const std::string str) { (void)str; return; }
+
+static bool looksLikeFloat(const std::string string) { (void)string; return true; }
+static void handleFloat(const std::string str) { (void)str; return; }
+
+static bool looksLikeDouble(const std::string string) { (void)string; return true; }
+static void handleDouble(const std::string str) { (void)str; return; }
+
 void ScalarConverter::convert(const std::string& param)
 {
-	std::cout << "[" << param << "]\n" ;
-	std::cout << "convert function called!\n";
+	if (looksLikeChar(param))
+		handleChar(param);
+	else if (looksLikeInt(param))
+		handleInt(param);
+	else if (looksLikeFloat(param))
+		handleFloat(param);
+	else if (looksLikeDouble(param))
+		handleDouble(param);
+/*	else if (isPseudoLiteral(param))
+		handlePseudo(param);*/
+	displayResult('a', 0, 0.0f, 0.0); // pass the conversion to this function
 }
 
 
