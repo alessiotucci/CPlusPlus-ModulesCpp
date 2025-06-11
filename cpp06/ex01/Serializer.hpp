bc034d33d9d672ef4fab62cae77b21ac9ed1f4ad
Alessio Tucci <alessiotucci33@gmail.com>
2025-06-11 13:04:53 +0200

    olk

--- cpp06/ex01/Serializer.hpp
+++ cpp06/ex01/Serializer.hpp
@@ -3,7 +3,7 @@
 /*   Host: e4r2p4.42roma.it                                           /_/     */
 /*   File: Serializer.hpp                                          ( o.o )    */
 /*   Created: 2025/05/30 15:24:49 | By: atucci <atucci@student.42  > ^ <      */
-/*   Updated: 2025/06/03 13:06:43                                   /         */
+/*   Updated: 2025/06/11 13:02:18                                   /         */
 /*   OS: Linux 6.5.0-44-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
 /*                                                                            */
 /* ************************************************************************** */
@@ -19,12 +19,12 @@ class Serializer
 {
 	// some stuff to change here
 private:
-public:
 	Serializer();
 	Serializer(const Serializer& other);
 	Serializer& operator=(const Serializer& other);
 	~Serializer();
 
+public:
 	static uintptr_t serialize(Data* ptr);
 //It takes a pointer and converts it to the unsigned integer type uintptr_t.
 	static Data* deserialize(uintptr_t raw);
