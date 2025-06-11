758fd0a9b70cbfb6c5c709e9123662cf5d8f6fdc
Alessio Tucci <alessiotucci33@gmail.com>
2025-06-11 13:04:17 +0200

    push day off

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -3,7 +3,7 @@
 /*   Host: e4r2p4.42roma.it                                           /_/     */
 /*   File: main.cpp                                                ( o.o )    */
 /*   Created: 2025/06/03 17:50:04 | By: atucci <atucci@student.42  > ^ <      */
-/*   Updated: 2025/06/05 17:16:51                                   /         */
+/*   Updated: 2025/06/11 12:55:28                                   /         */
 /*   OS: Linux 6.5.0-44-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
 /*                                                                            */
 /* ************************************************************************** */
@@ -19,24 +19,25 @@
 
 int main()
 {
-    std::srand(static_cast<unsigned int>(std::time(0)));
-    
-    for (int i = 0; i < 12; i++) {
-        std::cout << "\033[1;33m" << "===== TEST " << i + 1 << " =====" << "\033[0m" << std::endl;
-        
-        Base* obj = generate();
-        std::cout << std::endl;
-        
-        std::cout << "Identify by pointer: \033[1;36m";
-        identify(obj);
-        std::cout << "\033[0m";
-        
-        std::cout << "Identify by reference: \033[1;35m";
-        identify(*obj);
-        std::cout << "\033[0m" << std::endl;
-        
-        delete obj;
-    }
-    
-    return 0;
+	std::srand(static_cast<unsigned int>(std::time(0)));
+	
+	for (int i = 0; i < 12; i++)
+	{
+		std::cout << "\033[1;33m" << "===== TEST " << i + 1 << " =====" << "\033[0m" << std::endl;
+		
+		Base* obj = generate();
+		std::cout << std::endl;
+		
+		std::cout << "Identify by pointer: \033[1;36m";
+		identify(obj);
+		std::cout << "\033[0m";
+		
+		std::cout << "Identify by reference: \033[1;35m";
+		identify(*obj);
+		std::cout << "\033[0m" << std::endl;
+		
+		delete obj;
+	}
+	
+	return 0;
 }
