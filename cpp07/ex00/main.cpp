8ee1086decb6d76434bf03c32f2008b924930074
Alessio Tucci <alessiotucci33@gmail.com>
2025-06-13 14:00:29 +0200

    whatever

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -3,12 +3,29 @@
 /*   Host: e4r2p4.42roma.it                                           /_/     */
 /*   File: main.cpp                                                ( o.o )    */
 /*   Created: 2025/06/13 13:35:28 | By: atucci <atucci@student.42  > ^ <      */
-/*   Updated: 2025/06/13 13:35:41                                   /         */
+/*   Updated: 2025/06/13 13:58:39                                   /         */
 /*   OS: Linux 6.5.0-44-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
 /*                                                                            */
 /* ************************************************************************** */
+#include <iostream>
+#include <string>
+#include "whatever.hpp"
 
-int main()
-{
-	return (0);
+int main(void) {
+	int a = 2;
+	int b = 3;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+
+	return 0;
 }
+
