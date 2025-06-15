a571cadef090988ffb82f155b02a2f710c64a562
Alessio Tucci <alessiotucci33@gmail.com>
2025-06-15 19:30:24 +0200

    start working on simple iter

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -3,12 +3,19 @@
 /*   Host: e4r2p4.42roma.it                                           /_/     */
 /*   File: main.cpp                                                ( o.o )    */
 /*   Created: 2025/06/13 13:35:48 | By: atucci <atucci@student.42  > ^ <      */
-/*   Updated: 2025/06/13 13:36:02                                   /         */
+/*   Updated: 2025/06/15 19:30:04                                   /         */
 /*   OS: Linux 6.5.0-44-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
 int main()
 {
+	std::cout << "TEST1: Array of int\n";
+	std::cout << "TEST2: Array of double\n";
+	std::cout << "TEST3: Array of float\n";
+	std::cout << "TEST4: Array of char\n";
+	std::cout << "TEST5: Array of string\n";
 	return(0);
 }
