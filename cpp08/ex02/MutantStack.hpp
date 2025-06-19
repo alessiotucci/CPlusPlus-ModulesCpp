1887d112b596118ec35abc7de8a860680b45f8a6
Alessio Tucci <alessiotucci33@gmail.com>
2025-06-19 15:46:00 +0200

    empty stack mutant

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*   Host: e4r2p4.42roma.it                                           /_/     */
+/*   File: MutantStack.hpp                                         ( o.o )    */
+/*   Created: 2025/06/19 15:45:38 | By: atucci <atucci@student.42  > ^ <      */
+/*   Updated: 2025/06/19 15:45:50                                   /         */
+/*   OS: Linux 6.5.0-44-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef DEFAULT_HPP
+# define DEFAULT_HPP
+
+#include <string>
+
+class Default
+{
+	// some stuff to change here
+private:
+	std::string _name;
+	int _age;
+	bool _taken;
+
+public:
+	// 1. Default constructor
+	Default();
+	// 2. Copy constructor
+	Default(const Default& other);
+	// 3. Copy assignment operator
+	Default& operator=(const Default& other);
+	// 4. Destructor
+	~Default();
+	// No member variables or additional methods
+
+};
+#endif
+
+
