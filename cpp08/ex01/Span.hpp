94678ce594fb2b5a24bd2e24405e4445dd3a7d4f
Alessio Tucci <alessiotucci33@gmail.com>
2025-06-23 14:05:48 +0200

    start creating the main to test

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -3,7 +3,7 @@
 /*   Host: e4r2p4.42roma.it                                           /_/     */
 /*   File: Span.hpp                                                ( o.o )    */
 /*   Created: 2025/06/19 15:44:14 | By: atucci <atucci@student.42  > ^ <      */
-/*   Updated: 2025/06/23 13:54:08                                   /         */
+/*   Updated: 2025/06/23 14:03:05                                   /         */
 /*   OS: Linux 6.5.0-44-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
 /*                                                                            */
 /* ************************************************************************** */
@@ -11,6 +11,20 @@
 // Span.hpp
 #ifndef SPAN_HPP
 #define SPAN_HPP
+
+# define BLUE    "\033[1;34m"
+# define PURPLE  "\033[1;35m"
+# define GRAY    "\033[1;30m"
+# define GREEN   "\033[1;32m"
+# define CYAN    "\033[1;36m"
+# define YELLOW  "\033[1;33m"
+# define RED     "\033[1;31m"
+# define RESET   "\033[0m"
+# define BG_RESET        "\033[49m"
+# define BG_GREEN        "\033[42m"
+# define BG_CYAN         "\033[46m"
+# define BG_YELLOW       "\033[43m"
+# define BG_RED          "\033[41m"
 
 #include <vector>
 #include <stdexcept>
--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -3,7 +3,7 @@
 /*   Host: e4r2p4.42roma.it                                           /_/     */
 /*   File: main.cpp                                                ( o.o )    */
 /*   Created: 2025/06/19 15:16:53 | By: atucci <atucci@student.42  > ^ <      */
-/*   Updated: 2025/06/23 13:55:46                                   /         */
+/*   Updated: 2025/06/23 14:05:27                                   /         */
 /*   OS: Linux 6.5.0-44-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
 /*                                                                            */
 /* ************************************************************************** */
@@ -14,6 +14,15 @@
 
 int main()
 {
+	std::cout << GREEN << "SPAN: let's start testing span" << RESET << std::endl;
+	std::cout << YELLOW << "TEST1: " << RESET << "testing with "<< std::endl;
+	std::cout << YELLOW << "TEST2: " << RESET << "testing with "<< std::endl;
+	std::cout << YELLOW << "TEST3: " << RESET << "testing with "<< std::endl;
+	std::cout << YELLOW << "TEST4: " << RESET << "testing with "<< std::endl;
+	std::cout << YELLOW << "TEST5: " << RESET << "testing with "<< std::endl;
+	std::cout << YELLOW << "TEST6: " << RESET << "testing with "<< std::endl;
+	std::cout << YELLOW << "TEST7: " << RESET << "testing with "<< std::endl;
+	std::cout << GREEN << "SPAN: all test done" << RESET << std::endl;
 	return 0;
 }
 
