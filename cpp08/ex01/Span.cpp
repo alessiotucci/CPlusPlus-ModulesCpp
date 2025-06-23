d901e4b1d8d33b57f02b28512557a0d6e027c5aa
Alessio Tucci <alessiotucci33@gmail.com>
2025-06-23 13:56:04 +0200

    started the span with the right foot

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -3,7 +3,7 @@
 /*   Host: e4r2p4.42roma.it                                           /_/     */
 /*   File: Span.cpp                                                ( o.o )    */
 /*   Created: 2025/06/19 15:44:18 | By: atucci <atucci@student.42  > ^ <      */
-/*   Updated: 2025/06/23 13:19:18                                   /         */
+/*   Updated: 2025/06/23 13:55:21                                   /         */
 /*   OS: Linux 6.5.0-44-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
 /*                                                                            */
 /* ************************************************************************** */
@@ -19,11 +19,12 @@ Span::Span(const Span& other)
 	*this = other;
 }
 
-Span& Span::operator=(const Span& other)
+Span& Span::operator=(const Span &other)
 {
-	if (this != other)
+	if (this != &other)
 	{
-
+		this->_nSize = other._nSize;
+		this->_n = other._n;
 	}
 	return (*this);
 }
@@ -37,30 +38,32 @@ Any attempt to add a new element if there are already N elements stored should t
 */
 void Span::addNumber(int n)
 {
-
+//	if ()
+//	throw()
+	(void)n;
 }
 /*TODO:
 If there are no numbers stored, or only one, no span can be found. Thus, throw an exception.
 */
-int Span::shortestSpan() const { }
-int Span::longestSpan() const { }
+int Span::shortestSpan() const { return (0);}
+int Span::longestSpan() const { return (0);}
 
-const char	*Span::SpanIsFullException what() const throw()
+const char	*Span::SpanIsFullException::what() const throw()
 {
 	return("CUSTOM EXCEPTION: Span is full!!");
 }
 
-const char	*Span::SpanIsEmptyException what() const throw()
+const char	*Span::SpanIsEmptyException::what() const throw()
 {
 	return("CUSTOM EXCEPTION: Span is empty!!");
 }
 
-const char	*Span::SpanNotFoundException what() const throw()
+const char	*Span::SpanNotFoundException::what() const throw()
 {
 	return("CUSTOM EXCEPTION: Span not found!!");
 }
 
-const char	*Span::SpanIsOnlyOneException what() const throw()
+const char	*Span::SpanIsOnlyOneException::what() const throw()
 {
 	return("CUSTOM EXCEPTION: there only one number!!!");
 }
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -3,7 +3,7 @@
 /*   Host: e4r2p4.42roma.it                                           /_/     */
 /*   File: Span.hpp                                                ( o.o )    */
 /*   Created: 2025/06/19 15:44:14 | By: atucci <atucci@student.42  > ^ <      */
-/*   Updated: 2025/06/23 13:20:57                                   /         */
+/*   Updated: 2025/06/23 13:54:08                                   /         */
 /*   OS: Linux 6.5.0-44-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
 /*                                                                            */
 /* ************************************************************************** */
@@ -16,6 +16,7 @@
 #include <stdexcept>
 #include <algorithm> // sort, min_element, max_element
 #include <iterator>  // distance
+#include <iostream>
 
 
 class Span
@@ -64,7 +65,7 @@ public:
 	template <typename T>
 	void addRange(T begin, T end)
 	{
-
+		(void)begin;(void)end;
 	}
 };
 //overload to output
