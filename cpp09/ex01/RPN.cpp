c479643b3924dedd03089e16cba38517f1cf6233
Alessio Tucci <alessiotucci33@gmail.com>
2025-09-19 16:46:46 +0200

    starting to understanding c++

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -3,7 +3,7 @@
 /*   Host: atucci-Surface-Laptop-3                                    /_/     */
 /*   File: RPN.cpp                                                 ( o.o )    */
 /*   Created: 2025/06/21 14:03:45 | By: atucci <marvin@42.fr>      > ^ <      */
-/*   Updated: 2025/08/13 17:51:39                                   /         */
+/*   Updated: 2025/09/19 16:46:21                                   /         */
 /*   OS: Linux 6.8.0-59-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
 /*                                                                            */
 /* ************************************************************************** */
@@ -15,19 +15,30 @@ Rpn::Rpn()
 
 }
 
-Rpn::Rpn(const Rpn& other)
-{
-	(void)other;
-}
-
-Rpn& Rpn::operator=(const Rpn& other)
-{
-	(void)other;
-	return (*this);
-}
-
 Rpn::~Rpn()
 {
 
 }
+
+Rpn::Rpn(const Rpn& other)
+{
+	*this = other;
+	return;
+}
+
+std::stack<int> Rpn::getStack() const
+{
+	return (_Stack);
+}
+
+Rpn& Rpn::operator=(const Rpn& other)
+{
+	if (this != &other)
+	{
+		this->_Stack = other.getStack();
+	}
+	return (*this);
+}
+
+
 
--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -3,7 +3,7 @@
 /*   Host: atucci-Surface-Laptop-3                                    /_/     */
 /*   File: RPN.hpp                                                 ( o.o )    */
 /*   Created: 2025/06/21 14:03:41 | By: atucci <marvin@42.fr>      > ^ <      */
-/*   Updated: 2025/08/13 17:50:29                                   /         */
+/*   Updated: 2025/09/19 16:45:27                                   /         */
 /*   OS: Linux 6.8.0-59-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
 /*                                                                            */
 /* ************************************************************************** */
@@ -12,16 +12,33 @@
 # define RNP_HPP
 
 #include <iostream>
+#include <stack> // Required header for std::stack
+
+# define BLUE    "\033[1;34m"
+# define PURPLE  "\033[1;35m"
+# define GRAY    "\033[1;30m"
+# define GREEN   "\033[1;32m"
+# define CYAN    "\033[1;36m"
+# define YELLOW  "\033[1;33m"
+# define RED     "\033[1;31m"
+# define RESET   "\033[0m"
+# define BG_RESET        "\033[49m"
+# define BG_GREEN        "\033[42m"
+# define BG_CYAN         "\033[46m"
+# define BG_YELLOW       "\033[43m"
+# define BG_RED          "\033[41m"
+
 class Rpn
 {
-	// some stuff to change here
 private:
+	std::stack<int> _Stack;
 
 public:
 	Rpn();
 	Rpn(const Rpn& other);
 	Rpn& operator=(const Rpn& other);
 	~Rpn();
+	std::stack<int> getStack() const;
 
 };
 #endif
