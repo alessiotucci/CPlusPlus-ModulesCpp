467d9948d3423b6492a73246c30059c1b5c38bf7
Alessio Tucci <alessiotucci33@gmail.com>
2025-09-19 16:32:47 +0200

    start with the reverse polish notation

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -3,7 +3,7 @@
 /*   Host: atucci-Surface-Laptop-3                                    /_/     */
 /*   File: main.cpp                                                ( o.o )    */
 /*   Created: 2025/06/21 13:59:33 | By: atucci <marvin@42.fr>      > ^ <      */
-/*   Updated: 2025/08/13 17:51:53                                   /         */
+/*   Updated: 2025/09/19 16:32:24                                   /         */
 /*   OS: Linux 6.8.0-59-generic x86_64 | CPU: Intel(R) Core(TM) i (|_|)_)     */
 /*                                                                            */
 /* ************************************************************************** */
@@ -14,8 +14,12 @@
 int main(int ac, char **av)
 {
 	if (ac != 2)
+	{
 		std::cout << "RNP: wrong arguments" << std::endl;
+	}
 	else
+	{
 		std::cout << "RNP: evaluation expression [" << av[1] << "]" << std::endl;
+	}
 	return (0);
 }
